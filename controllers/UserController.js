467790803fbf50d13eac41321c80da064b358106
Igor299 <igorteixeira2305@gmail.com>
2024-11-29 07:41:37 +0100

    controllers

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,79 @@
-exports.home = async(req, res) => {
+exports.users = async (req, res) => {
     try {
-        res.render('home')
+        res.render('home'); // Rende a página home
     } catch (error) {
-     console.log(error);   
+        console.log(error);
     }
-}
+};
 
-exports.cadastro = async(req, res) => {
+exports.login = async (req, res) => {
     try {
-        res.render('cadastro')
+        res.render('Teladelogin'); // Rende a página de login
     } catch (error) {
-     console.log(error);   
+        console.log(error);
     }
-}
+};
 
-exports.login = async(req, res) => {
+exports.registerPage = async (req, res) => {
     try {
-        res.render('login')
+        res.render('cadastro'); // Rende a página de login
     } catch (error) {
-     console.log(error);   
+        console.log(error);
     }
-}
+};
+
+exports.Escolha = async (req, res) => {
+    try {
+        res.render('Escolha'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.Paginainicial = async (req, res) => {
+    try {
+        res.render('Paginainicial'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.cadastrocandidato = async (req, res) => {
+    try {
+        res.render('cadastrocandidato'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.cadastrorecrutador = async (req, res) => {
+    try {
+        res.render('cadastrorecrutador'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.Perfil = async (req, res) => {
+    try {
+        res.render('Perfil'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.Vaga = async (req, res) => {
+    try {
+        res.render('Vaga'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.PaginaRecrutadores = async (req, res) => {
+    try {
+        res.render('PaginaRecrutadores'); // Rende a Página escolha
+    } catch (error) {
+        console.log(error);
+    }
+};
